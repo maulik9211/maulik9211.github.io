1fc3737e5828ebc7d1ecc6ad4d4781c76eff4fbd
Maulik Mac <maulik9211@gmail.com>
2023-12-28 20:38:54 +0530

    Added deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://maulik9211.github.io',
+    base: '/maulik9211.github.io',
+
     integrations: [react(), astroImageTools],
     vite: {
         ssr: {
